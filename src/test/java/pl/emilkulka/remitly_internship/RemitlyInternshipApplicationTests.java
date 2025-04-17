a5402c10a0f47597d966b13617a12155a20e5d11
EmilKulka <226310@student.uek.krakow.pl>
2025-04-17 13:55:32 +0200

    build: project structure + dependencies + pipeline

---
+++ src/test/java/pl/emilkulka/remitly_internship/RemitlyInternshipApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.emilkulka.remitly_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class RemitlyInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
